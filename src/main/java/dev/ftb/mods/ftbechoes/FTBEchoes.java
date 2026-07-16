e9b48ec539b83ee623e1ce3c6ac620757c354007
Des Herriott <des.herriott@gmail.com>
2026-07-16 09:46:30 +0100

    chore: small cleanup for nbt editing

--- src/main/java/dev/ftb/mods/ftbechoes/FTBEchoes.java
+++ src/main/java/dev/ftb/mods/ftbechoes/FTBEchoes.java
@@ -4,7 +4,6 @@ import dev.ftb.mods.ftbechoes.command.FTBEchoesCommands;
 import dev.ftb.mods.ftbechoes.command.NBTEditCommand;
 import dev.ftb.mods.ftbechoes.datagen.DataGenerators;
 import dev.ftb.mods.ftbechoes.echo.EchoManager;
-import dev.ftb.mods.ftbechoes.echo.progress.TeamProgress;
 import dev.ftb.mods.ftbechoes.echo.progress.TeamProgressManager;
 import dev.ftb.mods.ftbechoes.net.SyncProgressMessage;
 import dev.ftb.mods.ftbechoes.registry.*;
@@ -15,11 +14,9 @@ import dev.ftb.mods.ftblibrary.integration.currency.CurrencyProvider;
 import dev.ftb.mods.ftblibrary.integration.stages.StageHelper;
 import dev.ftb.mods.ftblibrary.integration.stages.StageProvider;
 import dev.ftb.mods.ftblibrary.nbtedit.NBTEditResponseHandlers;
-import dev.ftb.mods.ftbteams.api.FTBTeamsAPI;
 import dev.ftb.mods.ftbteams.api.event.PlayerChangedTeamEvent;
 import dev.ftb.mods.ftbteams.api.event.PlayerLoggedInAfterTeamEvent;
 import dev.ftb.mods.ftbteams.api.event.TeamEvent;
-import net.minecraft.nbt.NbtOps;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
@@ -97,16 +94,7 @@ public class FTBEchoes {
     private void onServerAboutToStart(ServerAboutToStartEvent event) {
         EchoManager.initServer();
 
-        NBTEditResponseHandlers.INSTANCE.registerHandler(NBTEditCommand.FTBECHOES_PROGRESS, (serverPlayer, info, data) ->
-                FTBTeamsAPI.api().getManager().getTeamForPlayerID(info.getUUID("id")).ifPresent(team ->
-                        TeamProgress.CODEC.parse(NbtOps.INSTANCE, data)
-                                .ifSuccess(progress -> {
-                                    serverPlayer.displayClientMessage(Component.translatable("ftbechoes.message.progress_edited",
-                                            team.getColoredName()), false);
-                                    TeamProgressManager.get(event.getServer()).injectProgressData(team.getTeamId(), progress);
-                                })
-                )
-        );
+        NBTEditResponseHandlers.INSTANCE.registerHandler(NBTEditCommand.FTBECHOES_PROGRESS, NBTEditCommand::handleResponse);
     }
 
     private void onServerStopped(ServerStoppedEvent event) {
